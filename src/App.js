782ee81c631428a2e658b292c39f8bf8078358b7
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2019-12-09 17:52:25 -0500

    separated out TopRow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import TopRow from "./TopRow"
 import { getState } from "expect/build/jestMatchersObject";
 
 function App() {
+  
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-            <div className="home__score">{ homeScore }</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ awayScore }</div>
-          </div>
-        </div>
+        <TopRow homeScore={homeScore} awayScore={awayScore}/>
         <BottomRow />
       </section>
       <section className="buttons">
@@ -29,7 +21,7 @@ function App() {
           <button className="homeButtons__fieldGoal" onClick={handleHomeFieldGoalClick}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={handleAwayTouchdownClick}>>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={handleAwayTouchdownClick}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={handleAwayFieldGoalClick}>Away Field Goal</button>
         </div>
       </section>
